30bcdbffa29aa115761f66597e0a235a33656707
Max <max.schulzke@beyond.future>
2023-09-12 11:58:30 +0200

    update

--- snow.js
+++ snow.js
@@ -1,78 +1,73 @@
-		// Parameters for scale and speed
+// Number of snowflakes
+		const numSnowflakes = 40;
 		const scale = 1;
-		const speed = 8;
+		const deleteThreshhold = 120;
+		const rotationSpeed = 3;
 
-		// Create X number of SVG snowflakes
-		const X = 40;
-		for (let i = 0; i < X; i++) {
-			createSnowflake();
-		}
+		// Snowflake class to handle individual snowflake properties
+		class Snowflake {
+			constructor() {
+				this.init();
+			}
 
-		function createSnowflake() {
-			// Randomly select one of your SVG files
-			const svgIndex = Math.ceil(Math.random() * 12);
-			const svgFileName = `Artboard_${svgIndex}.svg`;
+			init() {
+				this.x = Math.random() * (window.innerWidth + 100) - 50;  // +/- 50px
+				this.y = -deleteThreshhold+5;  // Start off-screen, above the viewport
+				this.size = Math.random() * 10 + 5;
+				this.scale = Math.random() * 0.5 + 0.5; // New scale parameter
+				this.speed = Math.random() * 1 + 0.5;
+				this.angle = Math.random() * Math.PI * 2;
+				this.rotationSpeed = Math.random() * 1 - 0.5; // New rotation parameter
+				this.rotation = Math.random() * 360;
 
-			fetch(svgFileName)
-				.then(response => response.text())
-				.then(svgData => {
-					const div = document.createElement("div");
-					div.innerHTML = svgData;
-					const snowflake = div.querySelector("svg");
+				const randomSVG = Math.floor(Math.random() * 12) + 1; // 1 to 12
+				this.svgElement = document.createElement('img');
+				this.svgElement.src = `Artboard_${randomSVG}.svg`; // Assuming the SVG files are in the same directory
+				this.svgElement.classList.add('snowflake');
+				this.svgElement.style.width = `${100 * scale}px`;
+				this.svgElement.style.height = `${100 * scale}px`;
+				this.svgElement.style.left = `${this.x}px`;
+				this.svgElement.style.top = `${this.y}px`;
 
-					const rotationSpeed = 3 + Math.random() * 7;
-					const rotationDirection = Math.random() < 0.5 ? '' : '-';
+				document.body.appendChild(this.svgElement);
+			}
 
-					// Save attributes into snowflake object
-					snowflake.dataset.rotationSpeed = rotationSpeed;
-					snowflake.dataset.rotationDirection = rotationDirection;
+			update() {
+				this.y += Math.sin(this.angle) * this.speed;
+				this.x += Math.cos(this.angle) * this.speed;
+				this.angle += Math.random() * 0.05 - 0.025;
+				
+				// Update rotation
+				this.rotation += this.rotationSpeed * rotationSpeed;
+				console.log(this.rotation);
+				this.svgElement.style.transform = `rotate(${this.rotation}deg) scale(${this.scale})`;
 
-					// Add class for CSS styling
-					snowflake.classList.add("snowflake");
-
-					// Position and scale the SVG snowflake
-					snowflake.style.left = `${Math.random() * window.innerWidth}px`;
-					snowflake.style.top = `${Math.random() * window.innerHeight}px`;
-					snowflake.style.width = `${100 * scale}px`;
-					snowflake.style.height = `${100 * scale}px`;
-
-					// Add rotation
-					snowflake.style.animation = `rotate ${rotationSpeed}s infinite linear`;
-
-					// Add the initial rotation and rotation direction
-					snowflake.style.transform = `rotate(${Math.random() * 360}deg)`;
-
-					// Add SVG snowflake to the DOM
-					document.body.appendChild(snowflake);
-
-					// Animate the snowflake
-					animateSnowflake(snowflake);
-				});
-		}
-
-		function animateSnowflake(snowflake) {
-			let top = parseFloat(snowflake.style.top);
-			let left = parseFloat(snowflake.style.left);
-			const rotationSpeed = parseFloat(snowflake.dataset.rotationSpeed);
-			const rotationDirection = snowflake.dataset.rotationDirection;
-			const animation = setInterval(() => {
-				// Update position for smooth movement
-				top += (Math.random() - 0.5) * 1 * speed;
-				left += (Math.random() - 0.5) * 2 * speed;
-
-				// Set new position
-				snowflake.style.top = `${top}px`;
-				snowflake.style.left = `${left}px`;
-
-				// Update the rotation
-				snowflake.style.animation = `rotate ${rotationDirection}${rotationSpeed}s infinite linear`;
-
-				// Reset position when out of view
-				if (top > window.innerHeight) {
-					top = -100;
+				if (this.y > window.innerHeight + deleteThreshhold || this.y < -deleteThreshhold || this.x < -deleteThreshhold || this.x > window.innerWidth + deleteThreshhold) {
+					this.remove();
+					this.init();
 				}
-				if (left > window.innerWidth || left < -100) {
-					left = Math.random() * window.innerWidth;
-				}
-			}, Math.random() * 400);
+
+				this.svgElement.style.top = `${this.y}px`;
+				this.svgElement.style.left = `${this.x}px`;
+			}
+
+			remove() {
+				this.svgElement.remove();
+			}
 		}
+
+		// Initialize snowflakes
+		let snowflakes = [];
+		for (let i = 0; i < numSnowflakes; i++) {
+			snowflakes.push(new Snowflake());
+		}
+
+		// Update snowflakes
+		function updateSnowflakes() {
+			for (let snowflake of snowflakes) {
+				snowflake.update();
+			}
+			requestAnimationFrame(updateSnowflakes);
+		}
+
+		updateSnowflakes();
